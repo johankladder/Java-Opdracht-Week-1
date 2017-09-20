ef121ca14afa3a0899f592afbf084a30a27ff0a1
Johan Kladder <j.kladder@st.hanze.nl>
2017-09-20 13:30:13 +0200

    + Defined get requests

--- src/nl/hanze/web/t41/http/HTTPRespons.java
+++ src/nl/hanze/web/t41/http/HTTPRespons.java
@@ -1,98 +1,150 @@
 package nl.hanze.web.t41.http;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
+import com.sun.xml.internal.messaging.saaj.util.Base64;
+
+import java.io.*;
+import java.util.HashMap;
 
 public class HTTPRespons {
-	private OutputStream out;
-	private HTTPRequest request;
+    private OutputStream out;
+    private HTTPRequest request;
 
-	public HTTPRespons(OutputStream out) {
-		this.out = out;
-	}
+    public HTTPRespons(OutputStream out) {
+        this.out = out;
+    }
 
-	public void setRequest(HTTPRequest request) {
-		this.request = request;
-	}
+    public void setRequest(HTTPRequest request) {
+        this.request = request;
+    }
 
-	public void sendResponse() throws IOException {
-		byte[] bytes = new byte[HTTPSettings.BUFFER_SIZE];
-		FileInputStream fis = null;
-		String fileName = request.getUri();
+    public void sendResponse() throws IOException {
+        byte[] bytes = new byte[HTTPSettings.BUFFER_SIZE];
 
-		try {		
-			File file = new File(HTTPSettings.DOC_ROOT, fileName);			
-			FileInputStream inputStream = getInputStream (file);
-			
-			if (file.exists()) out.write(getHTTPHeader(fileName)); 
-			else out.write(getHTTPHeader(""));
-			
-			int ch = inputStream.read(bytes, 0, HTTPSettings.BUFFER_SIZE);
-			while (ch != -1) {
-				out.write(bytes, 0, ch);
-				ch = inputStream.read(bytes, 0, HTTPSettings.BUFFER_SIZE);
-			}
+        FileInputStream fis = null;
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (fis != null)
-				fis.close();
-		}
+        String fileName = request.getUri();
 
-	}
-	
-	private FileInputStream getInputStream (File file) {		
-		FileInputStream fis = null;
-		
-		/*
-		  *** OPGAVE 1.2 ***
-		  Stuur het bestand terug wanneer het bestaat; anders het standaard 404-bestand.
-		*/
-				
-		return fis;
-		
-	}
+        try {
+            File file = new File(HTTPSettings.DOC_ROOT, fileName);
+            FileInputStream inputStream = getInputStream(file);
 
-	private byte[] getHTTPHeader(String fileName) {
-		String fileType = getFileType(fileName);		
-		String header = "Date: " + HTTPSettings.getDate() + "Server: Barts eigen server\r\n";
+            if (file.exists()) out.write(getHTTPHeader(fileName));
+            else out.write(getHTTPHeader(""));
 
-		/*
-		 *** OPGAVE 1.4 en 1.5
-		   zorg voor een goede header:
-		   200 als het bestand is gevonden;
-		   404 als het bestand niet bestaat
-		   500 als het bestand wel bestaat maar niet van een ondersteund type is
-		   
-		   zorg ook voor ene juiste datum en tijd, de juiste content-type en de content-length.
-		*/
+            int ch = inputStream.read(bytes, 0, HTTPSettings.BUFFER_SIZE);
 
-		byte[] rv = header.getBytes();
-		return rv;
-	}
+            String fileType = getFileType(fileName);
+            if (fileType == "gif" || fileType == "png" || fileType == "jpeg" || fileType == "jpg" || fileType == "pdf")
+                bytes = Base64.encode(bytes);
 
-	private String getFileType(String fileName) {
-		int i = fileName.lastIndexOf(".");
-		String ext = "";
-		if (i > 0 && i < fileName.length() - 1) {
-			ext = fileName.substring(i + 1);
-		}
+            while (ch != -1) {
+                out.write(bytes, 0, ch);
+                ch = inputStream.read(bytes, 0, HTTPSettings.BUFFER_SIZE);
+            }
 
-		return ext;
-	}
 
-	private void showResponse(byte[] respons) {
-		StringBuffer buf = new StringBuffer(HTTPSettings.BUFFER_SIZE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null)
+                fis.close();
+        }
 
-		for (int i = 0; i < respons.length; i++) {
-			buf.append((char) respons[i]);
-		}
-		System.out.print(buf.toString());
+    }
 
-	}
+    private FileInputStream getInputStream(File file) {
+        try {
+            return new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            file = new File(HTTPSettings.DOC_ROOT, HTTPSettings.FILE_NOT_FOUND);
+            try {
+                return new FileInputStream(file);
+            } catch (FileNotFoundException f) {
+                f.printStackTrace();
+            }
+        }
+        return null;
+
+    }
+
+    private HashMap<String, String> getStatusMessage(String fileName, String fileType) {
+        try {
+            HashMap<String, String> data = new HashMap<>();
+            if ((new File(HTTPSettings.DOC_ROOT, fileName)).exists()) {
+                if (HTTPSettings.dataTypes.containsKey(fileType)) {
+                    data.put("filename", fileName);
+                    data.put("status", "200 OK");
+                } else {
+                    fileName = HTTPSettings.FILE_NOT_FOUND;
+                    data.put("filename", HTTPSettings.FILE_NOT_FOUND);
+                    data.put("status", "500 Internal Server Error");
+
+                }
+            } else {
+                fileName = HTTPSettings.FILE_NOT_FOUND;
+                data.put("filename", HTTPSettings.FILE_NOT_FOUND);
+                data.put("status", "404 File Not Found");
+            }
+
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private byte[] getHTTPHeader(String fileName) {
+
+        String fileType = getFileType(fileName);
+
+        HashMap data = getStatusMessage(fileName, fileType);
+
+        String statusMessage = (String) data.get("status");
+
+        fileName = (String) data.get("filename");
+
+        fileType = getFileType(fileName);
+
+        String header = null;
+
+        String contentType = "text/plain";
+        if (HTTPSettings.dataTypes.containsKey(fileType)) {
+            contentType = HTTPSettings.dataTypes.get(fileType);
+        }
+
+        int contentLength = (int) (new File(HTTPSettings.DOC_ROOT, fileName)).length();
+
+        header += ""
+                + "HTTP/1.1 " + statusMessage + "\r\n"
+                + "Content-Type: " + contentType + "\r\n"
+                + "Content-Length: " + contentLength + "\r\n"
+                + "Date: " + HTTPSettings.getDate() + "\r\n"
+                + "\r\n";
+
+
+        byte[] rv = header.getBytes();
+        return rv;
+    }
+
+    private String getFileType(String fileName) {
+        int i = fileName.lastIndexOf(".");
+        String ext = "";
+        if (i > 0 && i < fileName.length() - 1) {
+            ext = fileName.substring(i + 1);
+        }
+
+        return ext;
+    }
+
+
+    private void showResponse(byte[] respons) {
+        StringBuffer buf = new StringBuffer(HTTPSettings.BUFFER_SIZE);
+
+        for (int i = 0; i < respons.length; i++) {
+            buf.append((char) respons[i]);
+        }
+        System.out.print(buf.toString());
+
+    }
 
 }
--- src/nl/hanze/web/t41/http/HTTPSettings.java
+++ src/nl/hanze/web/t41/http/HTTPSettings.java
@@ -5,42 +5,52 @@ import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 public final class HTTPSettings {
-	// ZET HIER DE JUISTE DIRECTORY IN WAAR JE BESTANDEN STAAN.
-	
-	static public String DOC_ROOT = System.getProperty("user.dir");
-	static final String FILE_NOT_FOUND = "";
-	
-	static final int BUFFER_SIZE = 2048;	
-	static final int PORT_MIN=0;
-	static final int PORT_MAX=65535;
-	
-	static public int PORT_NUM = 4444;
-	static final HashMap<String, String> dataTypes = new HashMap<String, String>();	
+    // ZET HIER DE JUISTE DIRECTORY IN WAAR JE BESTANDEN STAAN.
 
-	static final String[] DAYS = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-	static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-	
-	public static void setPort(String[] args) {
-		if(args.length > 0) {
-			PORT_NUM = Integer.parseInt(args[0]);
-		}
-		// Else use the default.
-	}
+    static public String DOC_ROOT = System.getProperty("user.dir") + "/html";
+    static final String FILE_NOT_FOUND = "/404.html";
 
-	public static String getDate() {
-		GregorianCalendar cal = new GregorianCalendar();
-		String rv = "";
-		rv += DAYS[cal.get(Calendar.DAY_OF_WEEK) - 1] + ", ";
-		rv += cal.get(Calendar.DAY_OF_MONTH) + " " + MONTHS[cal.get(Calendar.MONTH)];
-		rv += " " + cal.get(Calendar.YEAR) + "\r\n";
+    static final int BUFFER_SIZE = 2048;
+    static final int PORT_MIN = 0;
+    static final int PORT_MAX = 65535;
 
-		return rv;
-	}
+    static public int PORT_NUM = 4444;
+    static final HashMap<String, String> dataTypes = new HashMap<String, String>() {{
+        put("html", "text/html");
+        put("css", "text/css");
+        put("js", "application/x-javascript");
+        put("txt", "text/plain");
+        put("gif", "image/gif");
+        put("png", "image/png");
+        put("jpg", "image/jpeg");
+        put("jpeg", "image/jpeg");
+        put("pdf", "application/pdf");
+    }};
 
-	public static void setDocumentRoot(String[] args) {
-		if(args.length > 1) {
-			DOC_ROOT = args[1];
-		}
-		// Else use the default.
-	}
+    static final String[] DAYS = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    public static void setPort(String[] args) {
+        if (args.length > 0) {
+            PORT_NUM = Integer.parseInt(args[0]);
+        }
+        // Else use the default.
+    }
+
+    public static String getDate() {
+        GregorianCalendar cal = new GregorianCalendar();
+        String rv = "";
+        rv += DAYS[cal.get(Calendar.DAY_OF_WEEK) - 1] + ", ";
+        rv += cal.get(Calendar.DAY_OF_MONTH) + " " + MONTHS[cal.get(Calendar.MONTH)];
+        rv += " " + cal.get(Calendar.YEAR) + "\r\n";
+
+        return rv;
+    }
+
+    public static void setDocumentRoot(String[] args) {
+        if (args.length > 1) {
+            DOC_ROOT = args[1];
+        }
+        // Else use the default.
+    }
 }
